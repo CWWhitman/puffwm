565a7285777907db9018e02db2436dbd2c33b4a2
Christopher Whitman <chris@cwwhitman.com>
2018-05-26 22:01:16 -0700

    error ❌

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #[macro_use]
 extern crate log;
+extern crate env_logger;
 
 mod window_manager;
 use window_manager::WindowManager;
 
 fn main() {
-    trace!("Creating window manager");
+    env_logger::init();
+
+    info!("Creating window manager");
     let wm = window_manager::WindowManager::new(WindowManager::create());
     wm.run();
 }
-
--- src/window_manager.rs
+++ src/window_manager.rs
@@ -1,10 +1,10 @@
 extern crate x11;
 
-use std::ptr::{
-    null,
-    null_mut,
-};
 use self::x11::xlib;
+use std::os::raw::c_int;
+use std::ptr::null;
+
+static mut WM_DETECTED: bool = false;
 
 pub struct WindowManager {
     display: *mut xlib::Display,
@@ -16,16 +16,15 @@ impl WindowManager {
         if !display.is_null() {
             WindowManager {
                 display: display,
-                root: unsafe { xlib::XDefaultRootWindow(display) as xlib::Window}
+                root: unsafe { xlib::XDefaultRootWindow(display) as xlib::Window },
             }
         } else {
             panic!("pointer to display was null");
         }
     }
 
-
     pub fn create() -> *mut xlib::Display {
-        trace!("Getting display pointer");
+        info!("Getting display pointer");
         let display;
         unsafe {
             display = xlib::XOpenDisplay(null());
@@ -38,16 +37,37 @@ impl WindowManager {
         display
     }
 
+    unsafe extern "C" fn on_wmdetected(display: *mut xlib::Display, error: *mut xlib::XErrorEvent) -> c_int {
+        assert_eq!((*error).error_code, xlib::BadAccess);
+        WM_DETECTED = true;
+        0
+    }
+
+
+    unsafe extern "C" fn on_xerror(display: *mut xlib::Display, error: *mut xlib::XErrorEvent) -> c_int {
+        error!("{:?}", (*error).error_code as u8);
+        0
+    }
+
     pub fn run(&self) {
-        /* nothing */
+        unsafe {
+            xlib::XSetErrorHandler(Some(WindowManager::on_wmdetected));
+            xlib::XSelectInput(self.display, self.root, xlib::SubstructureRedirectMask | xlib::SubstructureNotifyMask);
+            xlib::XSync(self.display, 0);
+            if (WM_DETECTED) {
+                error!("another window manager is running");
+                return;
+            }
+            xlib::XSetErrorHandler(Some(WindowManager::on_xerror));
+        }
     }
 }
 
 impl Drop for WindowManager {
     fn drop(&mut self) {
+        info!("display being dropped");
         unsafe {
             xlib::XCloseDisplay(self.display);
         }
     }
 }
-
